55b35642de371319f60cb8831cb689561140cbe4
Hashir-Durrani <muhammad.hashir.durrani@ocsbstudent.ca>
2021-03-24 18:52:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('submit').addEventListener('click', ages)
+
+let age = 0
+
+function ages () {
+  age = document.getElementById('age').value
+  age = parseInt(age)
+  day = document.getElementById('day').value
+  if (age >= 12 && age <= 14 && (day === 'Tuesday' || day ==='Thursday')) {
+    document.getElementById('reveal').innerHTML = 'Congrats! You get 50% off your admission because of student pricing.'
+  }
+  else {
+    document.getElementById('reveal').innerHTML = 'Sorry, you are not eligible for student pricing. You must pay the regular admission fee.'
+  }
+}
