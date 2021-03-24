141853f0b79e6d2d41b256481dea1f274d6cbc4d
Hashir-Durrani <muhammad.hashir.durrani@ocsbstudent.ca>
2021-03-24 19:00:43 +0000

    Made changes to Javascript based on Linter results.

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ document.getElementById('submit').addEventListener('click', ages)
 
 let age = 0
 
+let day = 0
+
 function ages () {
   age = document.getElementById('age').value
   age = parseInt(age)
   day = document.getElementById('day').value
-  if (age >= 12 && age <= 14 && (day === 'Tuesday' || day ==='Thursday')) {
+  if (age >= 12 && age <= 14 && (day === 'Tuesday' || day === 'Thursday')) {
     document.getElementById('reveal').innerHTML = 'Congrats! You get 50% off your admission because of student pricing.'
-  }
-  else {
+  } else {
     document.getElementById('reveal').innerHTML = 'Sorry, you are not eligible for student pricing. You must pay the regular admission fee.'
   }
 }
